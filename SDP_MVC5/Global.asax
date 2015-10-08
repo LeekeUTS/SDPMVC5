70b0d73a659b33f516437a754d9216c29c9be496
Leeke <leeke.zhou@gmail.com>
2015-10-08 19:00:31 +1100

    1st

---
+++ SDP_MVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SDP_MVC5.MvcApplication" Language="C#" %>
---
+++ SDP_MVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SDP_MVC5.Startup))]
+namespace SDP_MVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
