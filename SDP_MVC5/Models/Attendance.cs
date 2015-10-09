6d136e695853cab39e02ba6a43690ef710440b30
Leeke <leeke.zhou@gmail.com>
2015-10-09 14:37:34 +1100

    Attendence & Reminder

---
+++ SDP_MVC5/Models/Attendance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace SDP_MVC5.Models
+{
+    public class Attendance
+    {
+        public int ID { get; set; }
+        public int workshopID { get; set; }
+        public int studentID { get; set; }
+        public DateTime createdtime { get; set; }
+        public DateTime attendancetime { get; set; }
+        public string status { get; set; }
+    }
+
+    public class AttendanceDBContext : DbContext
+    {
+        public DbSet<Attendance> Attendances { get; set; }
+    }
+}
---
+++ SDP_MVC5/Models/Reminder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace SDP_MVC5.Models
+{
+    public class Reminder
+    {
+        public int ID { get; set; }
+        public int workshopID { get; set; }
+        public int studentID { get; set; }
+        public DateTime createdtime { get; set; }
+        public DateTime remindertime { get; set; }
+        public string status { get; set; }
+    }
+
+    public class ReminderDBContext : DbContext
+    {
+        public DbSet<Reminder> Reminders { get; set; }
+    }
+}
